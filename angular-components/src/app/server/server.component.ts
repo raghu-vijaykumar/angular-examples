afe99f355caccf604f390d8c1a742c4255848bba
Raghu D V <raghu.dv@boeing.com>
2019-12-06 16:29:20 +0530

    component usage in html

--- angular-components/src/app/server/server.component.ts
+++ angular-components/src/app/server/server.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 
 @Component({
+  /* selector: '.app-server' =>  using selector as a class *
+   * in HTML use as =>  <div class="app-server"> </div>    */
+  /* selector: 'app-server'                                *
+   * In HTML can be used as => <div app-server /> or <app-server /> */
   selector: 'app-server',
   // Inline template example
   template: `<p> server works! </p>`,
+  // Inline style example
   styles: [`
     p {
         color: blue;
